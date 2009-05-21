df4396ab86a919bbe55731abe3e3b96b171a4b40
root <root@exit237.tempdomain.com>
2009-05-21 04:31:39 +0000

    Added the ability to add additional track fields from the sessions page using javascript.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,7 @@
 module ProjectsHelper
+  def add_track_link(name)
+    link_to_function "Add a track" do |page|
+      page.insert_html :bottom, :tracks, :partial => "track", :object => Track.new
+    end
+  end
 end
