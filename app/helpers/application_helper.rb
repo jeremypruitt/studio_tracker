c833654f4bc0de4a2120ba590806b9669579a256
root <root@exit237.tempdomain.com>
2009-05-21 06:02:28 +0000

    Restyled the create links to use a superscripted plus-sign. Added a new mini nav and moved teh profile, logout, login, etc stuff there. Added some javascript to toggle the main nav area into a search box. Search functionality isn't there yet, but teh javascript to show the text filed is.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def toggle_search_link(name)
+    link_to_function "search" do |page|
+      page.replace_html :main_tabnav, :partial => "layouts/search"
+    end
+  end
 end
